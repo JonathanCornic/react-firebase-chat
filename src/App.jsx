f5e646eb32518e9726fd1818f7ee99b6ac862edb
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2024-05-07 22:53:14 +0200

    authentication firebase created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 import Chat from "./components/chat/chat";
 import Detail from "./components/detail/detail";
 import List from "./components/list/list";
 import Login from "./components/login/login";
 import Notification from "./components/notification/notification";
+import { auth } from "./lib/firebase/firebase";
 
 export default function App() {
-  const user = true;
+  const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <div className="container">
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,4 +1,12 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { auth, db } from "../../lib/firebase/firebase";
+import upload from "../../lib/firebase/upload";
 import "./login.css";
 
 export default function Login() {
@@ -6,6 +14,8 @@ export default function Login() {
     file: null,
     url: "",
   });
+
+  const [loading, setLoading] = useState(false);
 
   function handleAvatar(e) {
     if (e.target.files[0]) {
@@ -16,8 +26,49 @@ export default function Login() {
     }
   }
 
-  function handleLogin(e) {
+  async function handleRegister(e) {
     e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const imgUrl = await upload(avatar.file);
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
+      });
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
+      toast.success("Acount created!");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      toast.success("good")
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -27,13 +78,13 @@ export default function Login() {
         <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign In</button>
+          <button disabled={loading}>{loading ? "Loading" : "Sign In"}</button>
         </form>
       </div>
       <div className="separator"></div>
       <div className="item">
         <h2>Create an Account</h2>
-        <form onSubmit={handleLogin}>
+        <form onSubmit={handleRegister}>
           <label htmlFor="file">
             <img src={avatar.url || "./avatar.png"} alt="" />
             Upload an Image
@@ -42,7 +93,7 @@ export default function Login() {
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign Up</button>
+          <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>
         </form>
       </div>
     </div>
